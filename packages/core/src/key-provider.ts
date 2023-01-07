75475782fe4e0c13fc1bf250dec50a72be21adfd
Rémi Alvergnat <toilal.dev@gmail.com>
2023-01-07 17:20:03 +0100

    feat(core): add cache to key provider
    This avoid fetching keys on each token validation, while still working properly when keys are updated on the IdP.

--- packages/core/src/key-provider.ts
+++ packages/core/src/key-provider.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import { createMetadataService } from './metadata.js'
 import type { JWK } from 'jwk-to-pem'
 import jwkToPem from 'jwk-to-pem'
-import type { JwtClaims } from 'oidc-client-ts'
+import type { JwtClaims, SigningKey } from 'oidc-client-ts'
 
 export interface KeyProviderOptions {
   allowedIssuers: string[] | ((issuer: string) => boolean)
@@ -10,8 +10,19 @@ export interface KeyProviderOptions {
 
 export class KeyProviderError extends Error {}
 
+interface CacheEntry {
+  keys: SigningKey[],
+  pem: Map<string, string>
+}
+
 export class KeyProvider {
+  private keysCache: Map<string, CacheEntry> = new Map()
+
   constructor (private options: KeyProviderOptions) {
+  }
+
+  clearCache () {
+    this.keysCache.clear()
   }
 
   async getPublicKey (token: string): Promise<string> {
@@ -38,18 +49,56 @@ export class KeyProvider {
       throw new KeyProviderError(`Token issuer "${issuer}" is not allowed`)
     }
 
-    const metadataService = createMetadataService({ authority: issuer })
-    const keys = await metadataService.getSigningKeys()
-
-    if (!keys) {
-      throw new KeyProviderError(`No keys found for authority ${issuer}`)
+    let newKeys: SigningKey[] | undefined
+    let cacheEntry = this.getCacheEntry(issuer)
+    if (!cacheEntry) {
+      cacheEntry = await this.downloadAndCacheIssuerKeys(issuer)
     }
 
-    const matchingKey = keys.find(k => k.kid === decoded.header.kid)
+    if (!decoded.header.kid) {
+      throw new KeyProviderError('Token key id is not defined')
+    }
+
+    let matchingKey = this.findMatchingKey(cacheEntry.keys, decoded.header.kid)
+    if (!matchingKey && !newKeys) {
+      // Refresh keys from issuer source if no matching key is found and current keys were loaded from cache.
+      cacheEntry = await this.downloadAndCacheIssuerKeys(issuer)
+
+      matchingKey = this.findMatchingKey(cacheEntry.keys, decoded.header.kid)
+    }
+
     if (!matchingKey) {
-      throw new KeyProviderError(`No key matching kid=${decoded.header.kid} found for authority ${issuer}`)
+      throw new KeyProviderError(`No key matching key id "${decoded.header.kid}" found for issuer ${issuer}`)
     }
 
-    return jwkToPem(matchingKey as unknown as JWK)
+    let cachePem = cacheEntry.pem.get(decoded.header.kid)
+    if (!cachePem) {
+      cachePem = jwkToPem(matchingKey as unknown as JWK)
+      cacheEntry.pem.set(decoded.header.kid, cachePem)
+    }
+
+    return cachePem
+  }
+
+  private async downloadAndCacheIssuerKeys (issuer: string): Promise<CacheEntry> {
+    const metadataService = createMetadataService({ authority: issuer })
+    const newKeys = await metadataService.getSigningKeys()
+
+    if (!newKeys) {
+      throw new KeyProviderError(`No keys found for issuer ${issuer}`)
+    }
+
+    const newCacheEntry = { keys: newKeys, pem: new Map() }
+    this.keysCache.set(issuer, newCacheEntry)
+
+    return newCacheEntry
+  }
+
+  private getCacheEntry (issuer: string): CacheEntry | undefined {
+    return this.keysCache.get(issuer)
+  }
+
+  private findMatchingKey (keys: SigningKey[], kid: string) {
+    return keys.find(key => typeof key.kid === 'string' ? key.kid === kid : key.kid.includes(kid))
   }
 }
