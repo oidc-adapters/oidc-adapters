45ee80649cca89f03eac39be2f0c9c6a2d523972
Rémi Alvergnat <toilal.dev@gmail.com>
2023-01-13 14:32:10 +0100

    feat(core): add PermissionsProvider interface

---
+++ packages/core/src/permissions.ts
@@ -0,0 +1,9 @@
+export interface PermissionsProvider {
+  hasPermission (permission: string): boolean | Promise<boolean>
+
+  hasResourcePermission (resource: string, permission: string): boolean | Promise<boolean>
+
+  getPermissions (): string[] | Promise<string[]>
+
+  getResourcePermissions (resource: string): string[] | Promise<string[]>
+}
